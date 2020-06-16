057d417f083d7e2c45cd643bd2bbd0a144bd6667
Jesse Visser <j.w.visser-3@student.utwente.nl>
2020-06-16 11:18:29 +0200

    Added random Y rotation to bobbing

--- Assets/Scripts/Bobbing.cs
+++ Assets/Scripts/Bobbing.cs
@@ -2,23 +2,29 @@
 
 public class Bobbing : MonoBehaviour
 {
-    float floatY;
-    float originalY;
-    float offset;
+    float posY;
+    float rotY;
+    float originalPosY;
+    float originalRotY;
+    float rndOffset;
 
-    public float floatStrength = 1;
+    public float floatStrength = 0.1f;
+    public float rotateStrength = 0.05f;
 
     void Start()
     {
-        offset = Random.Range(0.5f, 1.5f);
-        originalY = transform.position.y;
+        rndOffset = Random.Range(0.5f, 1.5f);
+        originalPosY = transform.position.y;
+        originalRotY = transform.rotation.y;
     }
 
     void Update()
     {
         Vector3 pos = transform.position;
-        floatY = pos.y;
-        floatY = originalY + (Mathf.Sin(Time.time * offset) * floatStrength);
-        transform.position = new Vector3(pos.x, floatY, pos.z);
+        Quaternion rot = transform.rotation;
+        posY = originalPosY + (Mathf.Sin(Time.time * rndOffset) * floatStrength);
+        rotY = originalRotY + (Mathf.Sin(Time.time * rndOffset) * rotateStrength);
+        transform.position = new Vector3(pos.x, posY, pos.z);
+        transform.rotation = new Quaternion(rot.x, rotY, rot.z, rot.w);
     }
 }
